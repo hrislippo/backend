116b3d04a78742841a40f1cff863b3c32c31d536
Albert <albert@lippo.co.id>
2026-05-05 17:03:20 +0700

    disable cors

--- src/main/java/lippo/hris/system/config/SecurityConfig.java
+++ src/main/java/lippo/hris/system/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
+                .cors(cors -> cors.disable())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/authentication/login").permitAll()
--- src/main/java/lippo/hris/system/config/WebConfig.java
+++ src/main/java/lippo/hris/system/config/WebConfig.java
@@ -9,11 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 
 }
